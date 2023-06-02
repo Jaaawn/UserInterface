3169c960ca27b429238879351a079f987412fc59
Rosemarie Peerless <rpeerless@student.unimelb.edu.au>
2023-06-02 11:50:18 +1000

    fix slideshow and buttons

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-let slideIndex = 0;
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function shiftSlide(n) {
-    const slides = document.querySelectorAll('.carousel-images img');
-    slideIndex = (slideIndex + n) % slides.length;
-    if (slideIndex < 0) {
-        slideIndex = slides.length - 1;
-    }
-    slides.forEach(slide => slide.classList.remove('active'));
-    slides[slideIndex].classList.add('active');
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    shiftSlide(0);
-});
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
